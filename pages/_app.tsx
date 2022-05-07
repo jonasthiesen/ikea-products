72e64d4791ff213b9362d8e5c6213b5ad0a8eae7
Jonas Thiesen <jonas.k.thiesen@gmail.com>
2022-05-07 13:24:44 +0200

    Add page with overview of all products with full text search
    Using axios and react-query to fetch the data exposed in the API. Using the URL as state to make it easy to "store" a search by just copying the URL.
    
    Has page restoration enabled so you get to the correct spot on the page when you go back.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type {AppProps} from "next/app"
+import {QueryClient, QueryClientProvider} from "react-query"
+import {ReactQueryDevtools} from "react-query/devtools"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      useErrorBoundary: true,
+    },
+  },
+})
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/products/[productId].tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+import type {NextPage} from "next"
+
+const Index: NextPage = () => {
+  return <div>Hello</div>
+}
+
+export default Index
